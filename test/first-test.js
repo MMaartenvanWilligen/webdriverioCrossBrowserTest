fc511946b0044f40daefe400d9ae9912d99601fc
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-06 14:33:17 +0200

    second commit 5

--- test/first-test.js
+++ test/first-test.js
@@ -79,3 +79,24 @@
 //         return driver.quit();
 //     });
 // });
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'chrome',
+        version: '27.0',
+        platform: 'XP',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        name: 'integration',
+        build: process.env.TRAVIS_BUILD_NUMBER
+    }
+};
+
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function(title) {
+    console.log('Title was: ' + title);
+})
+    .end();
+
