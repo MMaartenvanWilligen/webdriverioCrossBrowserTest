03a29c57aae48c77ea0a47b548eb703f60d4b7e7
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-07 16:34:48 +0200

    wdio sauce 2

--- test/first-test.js
+++ test/first-test.js
@@ -80,29 +80,29 @@
 //     // });
 // });
 
-var webdriverio = require('webdriverio'),
-    client = browser.remote({
-        desiredCapabilities: {
-            name: 'This is an example test',
-            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-            build: process.env.TRAVIS_BUILD_NUMBER,
+// var webdriverio = require('webdriverio'),
+//     client = browser.remote({
+//         desiredCapabilities: {
+//             name: 'This is an example test',
+//             'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+//             build: process.env.TRAVIS_BUILD_NUMBER,
+//
+//             // If using Open Sauce (https://saucelabs.com/opensauce/),
+//             // capabilities must be tagged as "public" for the jobs's status
+//             // to update (failed/passed). If omitted on Open Sauce, the job's
+//             // status will only be marked "Finished." This property can be
+//             // be omitted for commerical (private) Sauce Labs accounts.
+//             // Also see https://support.saucelabs.com/customer/portal/articles/2005331-why-do-my-tests-say-%22finished%22-instead-of-%22passed%22-or-%22failed%22-how-do-i-set-the-status-
+//             'public': true
+//         },
+//         host: 'ondemand.saucelabs.com',
+//         port: 80,
+//         user: process.env.SAUCE_USERNAME,
+//         key: process.env.SAUCE_ACCESS_KEY,
+//         logLevel: 'silent'
+//     }).init();
 
-            // If using Open Sauce (https://saucelabs.com/opensauce/),
-            // capabilities must be tagged as "public" for the jobs's status
-            // to update (failed/passed). If omitted on Open Sauce, the job's
-            // status will only be marked "Finished." This property can be
-            // be omitted for commerical (private) Sauce Labs accounts.
-            // Also see https://support.saucelabs.com/customer/portal/articles/2005331-why-do-my-tests-say-%22finished%22-instead-of-%22passed%22-or-%22failed%22-how-do-i-set-the-status-
-            'public': true
-        },
-        host: 'ondemand.saucelabs.com',
-        port: 80,
-        user: process.env.SAUCE_USERNAME,
-        key: process.env.SAUCE_ACCESS_KEY,
-        logLevel: 'silent'
-    }).init();
-
-client
+browser
     .init()
     .url('http://localhost:4445/webdriverioProject/website/index.html')
     .getTitle().then(console.log)
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,6 @@
+var BUILD_ID = process.env.TRAVIS_BUILD_ID || new Date().getTime();
+
+
 exports.config = {
 
     /**
@@ -6,12 +9,41 @@ exports.config = {
     capabilities: [{
         browserName: 'chrome',
         version: 'latest',
-        platform: "windows 10"
+        platform: "windows 10",
+        tags: ['examples'],
+        name: 'This is an example test',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
     }, {
         browserName: "firefox",
         version: 'latest',
-        platform: "windows 10"
+        platform: "windows 10",
+        tags: ['examples'],
+        name: 'This is an example test',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
     }],
+    /**
+     * test configurations
+     */
+
+    user: "Maaktnietzoveeluit",
+    key: "519926b3-9bcf-4500-9725-eb09b0a50d5d",
+    logLevel: 'silent',
+    coloredLogs: true,
+    screenshotPath: 'screenshots',
+    waitforTimeout: 10000,
+    reporters: ['dot'],
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd'
+    },
+    reporterOptions: {
+        outputDir: './'
+    },
+    /**
+     * specify test files
+     */
     specs: [
         './test/first-test.js'
     ]
