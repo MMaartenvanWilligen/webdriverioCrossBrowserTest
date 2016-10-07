4e41eae672fcb15ffafc89e5413e2d2bd4e80b43
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-07 15:03:54 +0200

    conf connect 8

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,8 +19,13 @@ exports.config = {
     coloredLogs: true,
     screenshotPath: 'screenshots',
     waitforTimeout: 10000,
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     build: process.env.TRAVIS_BUILD_NUMBER,
+    'public': true,
+    name: 'This is an example test',
+    port: 80,
     reporters: ['dot'],
     framework: 'mocha',
     mochaOpts: {
