96e7a2776bc4c506c53597bbf94178e19f6186e0
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-10 12:20:52 +0200

    webdriverio working whitout cross browser

--- test/first-test.js
+++ test/first-test.js
@@ -80,33 +80,33 @@
 //     // });
 // });
 
-// var webdriverio = require('webdriverio'),
-//     client = webdriverio.remote({
-//         desiredCapabilities: {
-//             browserName: 'chrome',
-//             version: '27',
-//             platform: 'XP',
-//             tags: ['examples'],
-//             name: 'This is an example test',
-//             'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-//             build: process.env.TRAVIS_BUILD_NUMBER,
-//
-//
-//             // If using Open Sauce (https://saucelabs.com/opensauce/),
-//             // capabilities must be tagged as "public" for the jobs's status
-//             // to update (failed/passed). If omitted on Open Sauce, the job's
-//             // status will only be marked "Finished." This property can be
-//             // be omitted for commerical (private) Sauce Labs accounts.
-//             // Also see https://support.saucelabs.com/customer/portal/articles/2005331-why-do-my-tests-say-%22finished%22-instead-of-%22passed%22-or-%22failed%22-how-do-i-set-the-status-
-//             'public': true
-//         },
-//         host: 'ondemand.saucelabs.com',
-//         port: 80,
-//         user: process.env.SAUCE_USERNAME,
-//         key: process.env.SAUCE_ACCESS_KEY,
-//         logLevel: 'silent'
-//     }).init();
-//
+var webdriverio = require('webdriverio'),
+    client = webdriverio.remote({
+        desiredCapabilities: {
+            browserName: 'chrome',
+            version: '27',
+            platform: 'XP',
+            tags: ['examples'],
+            name: 'This is an example test',
+            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+            build: process.env.TRAVIS_BUILD_NUMBER,
+
+
+            // If using Open Sauce (https://saucelabs.com/opensauce/),
+            // capabilities must be tagged as "public" for the jobs's status
+            // to update (failed/passed). If omitted on Open Sauce, the job's
+            // status will only be marked "Finished." This property can be
+            // be omitted for commerical (private) Sauce Labs accounts.
+            // Also see https://support.saucelabs.com/customer/portal/articles/2005331-why-do-my-tests-say-%22finished%22-instead-of-%22passed%22-or-%22failed%22-how-do-i-set-the-status-
+            'public': true
+        },
+        host: 'ondemand.saucelabs.com',
+        port: 80,
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY,
+        logLevel: 'silent'
+    }).init();
+
 
 var webdriver = require("webdriverio");
 var until = webdriver.until;
@@ -120,7 +120,11 @@ describe("functional testing in real browser", function () {
     var driver = require("./driver").GetDriver();
 
     it("Expect onclick text change to buttontransform", function (done) {
+<<<<<<< Updated upstream
         driver
+=======
+        client
+>>>>>>> Stashed changes
             .url('http://localhost:4445/webdriverioProject/website/index.html')
             .setValue('*[name="q"]','webdriverio')
             .click('*[name="btnG"]')
