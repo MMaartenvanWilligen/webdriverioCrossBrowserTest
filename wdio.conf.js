f1e6bcdf537b40ed0a381f57fd5746956996fa70
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-10 09:17:21 +0200

    wdio sauce runner 4

--- wdio.conf.js
+++ wdio.conf.js
@@ -30,6 +30,7 @@ exports.config = {
 
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
+    services:["sauce"],
     logLevel: 'silent',
     coloredLogs: true,
     screenshotPath: 'screenshots',
