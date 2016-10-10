6ab86700e809a0f27291dd5412d77be31435a39e
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-10 10:50:13 +0200

    sauce-connect-runner

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,22 +1,25 @@
+var sauceConnectLauncher = require('sauce-connect-launcher');
+global.sauceConnectProcess = null;
+
 exports.config = {
 
     /**
      * capabilities
      */
-/*
-    commonCapabilities: {
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        build: process.env.TRAVIS_BUILD_NUMBER
-    },*/
+    /*
+     commonCapabilities: {
+     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+     build: process.env.TRAVIS_BUILD_NUMBER
+     },*/
 
     capabilities: [{
         browserName: 'chrome',
         version: 'latest',
         platform: "windows 10",
         tags: ['examples'],
-        name: 'chrome test',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        build: process.env.TRAVIS_BUILD_NUMBER
+        name: 'chrome test'
+        /*'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+         build: process.env.TRAVIS_BUILD_NUMBER*/
     }, {
         browserName: "firefox",
         version: 'latest',
@@ -27,10 +30,10 @@ exports.config = {
     /**
      * test configurations
      */
-    port: 4445,
-    /*host: 'http://' + "Maaktnietzoveeluit" + ':' + "519926b3-9bcf-4500-9725-eb09b0a50d5d" + '@ondemand.saucelabs.com:80/wd/hub',*/
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
+    /*port: 4445,
+     /!*host: 'http://' + "Maaktnietzoveeluit" + ':' + "519926b3-9bcf-4500-9725-eb09b0a50d5d" + '@ondemand.saucelabs.com:80/wd/hub',*!/
+     user: process.env.SAUCE_USERNAME,
+     key: process.env.SAUCE_ACCESS_KEY,*/
     /*services:["sauce"],*/
     logLevel: 'silent',
     coloredLogs: true,
@@ -49,6 +52,22 @@ exports.config = {
      */
     specs: [
         './test/first-test.js'
-    ]
+    ],
+    onPrepare: function () {
+        return new Promise(function (resolve, reject) {
+            sauceConnectLauncher({
+                username: 'Maaktnietzoveeluit',
+                accessKey: '27dde83a-1cf7-450d-8c88-857c4d3cde43'
+            }, function (err, sauceConnectProcess) {
+                if (err) {
+                    return reject(err);
+                }
+                console.log('conexion realizada');
+                global.sauceConnectProcess = sauceConnectProcess
+                resolve();
+            });
+        });
+
+    }
 };
 
