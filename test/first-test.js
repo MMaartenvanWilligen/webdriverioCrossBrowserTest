11aa27179059dc61b2720561e8a78428d915c3d3
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-11 14:59:40 +0200

    pageobject gettext method

--- test/first-test.js
+++ test/first-test.js
@@ -13,23 +13,21 @@ describe('Home page', function () {
     it("should open the homepage", function () {
         homepage = new Home();
         return homepage.getUrl().then(function () {
-           return homepage.currentUrl().then(function (url) {
-               return assert.equal(url, homepage.urllocal);
+            return homepage.currentUrl().then(function (url) {
+                return assert.equal(url, homepage.urllocal);
             })
         });
     });
 
     it("The title is 'Home Page'", function () {
         return browser.getTitle().then(function (title) {
-           return assert.equal(title, "Home Page");
+            return assert.equal(title, "Home Page");
         });
     });
 
     it("CTA button should have text raised", function () {
-        return homepage.ctaButton().then(function () {
-           return browser.getText().then(function (txt) {
-               return assert.equal(txt, "RAISED");
-            });
+        homepage.ctaButtonText().then(function (txt) {
+            return assert.equal(txt, "RAISED");
         });
     });
 });
--- test/page-objects/homePage.js
+++ test/page-objects/homePage.js
@@ -14,40 +14,33 @@ var Page = require("./page");
  * @param driver
  * */
 
+
 function Home() {
 
     Page.call(this, "http://localhost:8000/website/index.html");
+    this.ctaButton = "#ctaButton";
+
 }
+
 
 // subclass Homepage extends page
 Home.prototype = Object.create(Page.prototype);
 Home.prototype.constructor = Home;
 
-/* @desc method get value header
- * need to manually handle the Promise so it can be handled by the mocha framework in first-test.js
- * @return promise of text header
- * */
-
-
-
-/* @desc find element raised button
- * need to manually handle the Promise so it can be handled by the mocha framework in first-test.js
- * @return promise raised button element
- * */
-
-Home.prototype.ctaButton = function () {
-    return browser.element("#ctaButton");
-};
 
 /* @desc method click on raised button
  * need to manually handle the Promise so it can be handled by the mocha framework in first-test.js
  * @return promise raised button element
  * */
-Home.prototype.ctaButtonClick = function () {
-        this.ctaButton().then(function () {
-            browser.click();
+
+Home.prototype.ctaButtonText = function () {
+        browser.getText(this.ctaButton).then(function (txt) {
+            return txt;
         })
-        
+};
+
+Home.prototype.ctaButtonClick = function () {
+    return browser.click(this.ctaButton);
 };
 
 /*
