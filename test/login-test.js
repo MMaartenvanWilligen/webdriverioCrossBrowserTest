ec7c0ba30af83d0918b6d38ce03d05aebbf5abd1
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-12 10:13:08 +0200

    login test change syntax

--- test/login-test.js
+++ test/login-test.js
@@ -2,14 +2,12 @@
  * Created by maarten on 04-10-16.
  */
 
-
 var assert = require("assert");
 var chai = require('chai')
     , expect = chai.expect
     , should = chai.should();
 
 var Login = require("./page-objects/loginPage");
-
 
 /*
  * declaring variables
@@ -43,6 +41,36 @@ describe("Login test", function () {
     it("The title should be 'Login page'", function () {
         return browser.getTitle().then(function (title) {
             return assert.equal(title, "Login page");
+        });
+    });
+
+    it("should set value username field to 'Admin'", function () {
+        return loginpage.inputUsernameSetValue("Admin").then(function () {
+            return loginpage.inputUsernameGetValue().then(function (value) {
+                assert.equal(value, "Admin");
+            });
+        });
+    });
+
+    it("should set value password field to 'Password'", function () {
+        return loginpage.inputPasswordSetValue("Password").then(function () {
+            return loginpage.inputPasswordGetValue().then(function (value) {
+                assert.equal(value, "Password");
+            });
+        });
+    });
+
+    it("should not show texfield-errors", function () {
+        return loginpage.errorHandlingSpanDisplayed().then(function (bool) {
+                return assert.equal(false, bool)
+        })
+    });
+
+    it("after submit should go to index", function () {
+        return loginpage.submitClick().then(function () {
+            return browser.getTitle().then(function (title) {
+               return assert.equal(title, "Home Page");
+            });
         });
     });
 
--- test/page-objects/loginPage.js
+++ test/page-objects/loginPage.js
@@ -13,6 +13,10 @@ var Page = require("./page");
 function Login() {
 
     Page.call(this, "http://localhost:8000/website/loginPage.html");
+    this.inputUsername = "#username";
+    this.inputPassword = "#userpass";
+    this.submitButton = "#submit";
+    this.ErrorHandlingFormSpan = ".mdl-textfield__error";
 
 }
 
@@ -20,35 +24,18 @@ function Login() {
 Login.prototype = Object.create(Page.prototype);
 Login.prototype.constructor = Login;
 
-
 /* @desc method get input field username
  * need to manually handle the Promise so it can be handled by the mocha framework in first-test.js
  * @return promise of element username field
  * */
 
-Login.prototype.inputUsername = function () {
-
-    browser.element(webdriver.By.id("username")).then(function (elm) {
-
-    });
-
-
-    //inputUserName.sendKeys('Maarten');
-};
 
 /* @desc method get value of input field username
  * need to manually handle the Promise so it can be handled by the mocha framework in first-test.js
  * @return promise value username
  * */
 Login.prototype.inputUsernameGetValue = function () {
-
-    var d = webdriver.promise.defer();
-    this.inputUsername().then(function (elm) {
-        elm.getAttribute("value").then(function (value) {
-            d.fulfill(value);
-        });
-    });
-    return d.promise;
+    return browser.getValue(this.inputUsername);
 };
 
 /* @desc method set input field username
@@ -56,75 +43,33 @@ Login.prototype.inputUsernameGetValue = function () {
  * @return promise
  * */
 Login.prototype.inputUsernameSetValue = function (inputText) {
-    var d = webdriver.promise.defer();
-    this.inputUsername().then(function (elm) {
-        elm.sendKeys(inputText);
-        d.fulfill(elm);
-    });
-    return d.promise;
-};
-
-Login.prototype.inputPassword = function () {
-    var d = webdriver.promise.defer();
-    this.driver.findElement(webdriver.By.id("userpass")).then(function (elm) {
-        d.fulfill(elm);
-    });
-    return d.promise;
+    return browser.setValue(this.inputUsername, inputText);
 };
 
 Login.prototype.inputPasswordGetValue = function () {
-    var d = webdriver.promise.defer();
-    this.inputPassword().then(function (elm) {
-        elm.getAttribute("value").then(function (value) {
-            d.fulfill(value);
-        });
-    });
-    return d.promise;
+    return browser.getValue(this.inputPassword);
 };
 
 Login.prototype.inputPasswordSetValue = function (inputText) {
-    var d = webdriver.promise.defer();
-    this.inputPassword().then(function (elm) {
-        elm.sendKeys(inputText);
-        d.fulfill(elm);
-    });
-    return d.promise;
-};
-
-Login.prototype.submitButton = function () {
-    var d = webdriver.promise.defer();
-    this.driver.findElement(webdriver.By.id('submit')).then(function (elm) {
-        d.fulfill(elm);
-    });
-    return d.promise;
+    return browser.setValue(this.inputPassword, inputText);
 };
 
 Login.prototype.submitClick = function () {
-    var d = webdriver.promise.defer();
-    this.submitButton().then(function (elm) {
-        elm.click();
-        d.fulfill(elm);
-    });
-    return d.promise
-};
 
-Login.prototype.ErrorHandlingFormSpan = function () {
-    var d = webdriver.promise.defer();
-    this.driver.findElement(webdriver.By.className("mdl-textfield__error")).then(function (elm) {
-        d.fulfill(elm);
-    });
-    return d.promise;
+    browser.click(this.submitButton);
 };
 
 Login.prototype.loginProcess = function (Username, password) {
-    var d = webdriver.promise.defer();
+
     this.inputUsernameSetValue(Username);
     this.inputPasswordSetValue(password);
     this.submitClick().then(function () {
-        d.fulfill();
-    });
 
-    return d.promise;
+    });
+};
+
+Login.prototype.errorHandlingSpanDisplayed = function () {
+    return browser.isVisible(this.ErrorHandlingFormSpan);
 };
 
 module.exports = Login;
