82543c88a5c9a8cd03e910d3c1399c80aae333f0
MMaartenvanWilligen <maarten@connectholland.nl>
2016-10-17 16:00:46 +0200

    make merge ready

--- wdio.conf.js
+++ wdio.conf.js
@@ -31,7 +31,7 @@ exports.config = {
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
     port: 80,
-    logLevel: 'error',
+    logLevel: 'silent',
     sync: false,
     coloredLogs: true,
     waitforTimeout: 10000,
